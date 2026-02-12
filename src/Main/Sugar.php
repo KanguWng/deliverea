72a84fc73653b27aa58002361ade960f9d32c76a
Kangu <luxykww@gmail.com>
2026-02-12 18:44:29 +0100

    Create Sugar.php

---
+++ src/Main/Sugar.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Deliverea\CoffeeMachine\Main\Sugar;
+
+final class Sugar
+{
+    //Definimos el número de azúcares y si se necesita o no un palito
+    private int $sugar;
+    private bool $stick;
+
+    //Hacemos un constructor que valide el número de azúcares y asigne si se necesita o no un palito
+    public function __construct ($sugar) {
+        if ($sugar < 0 || $sugar > 2){
+            throw new \InvalidArgumentException('The number of sugars should be between 0 and 2.');
+        }
+        $this->sugar = $sugar;
+        $this->stick = $sugar >= 1; 
+    }
+
+    public function getSugar() {
+        return $this->sugar;
+    }
+
+    public function hasStick() { 
+        return $this->stick; 
+    }
+}
